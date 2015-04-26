48b14571dbee5d1fbb44ae583d061cbfb596f0db
David Benque <dbenque@gmail.com>
2015-04-26 23:58:00 +0200

    remove indirection on appengine.Context interface

--- datastoreEntity/datastoreEntity.go
+++ datastoreEntity/datastoreEntity.go
@@ -13,48 +13,48 @@ type DatastoreEntity interface {
 	GetKind() string
 }
 
-func Delete(context *appengine.Context, entity DatastoreEntity) error {
-	key := datastore.NewKey(*context, (entity).GetKind(), (entity).GetKey(), 0, nil)
+func Delete(context appengine.Context, entity DatastoreEntity) error {
+	key := datastore.NewKey(context, (entity).GetKind(), (entity).GetKey(), 0, nil)
 
-	if err := datastore.Delete(*context, key); err != nil {
-		(*context).Infof("Error while removing entity  %v", entity)
+	if err := datastore.Delete(context, key); err != nil {
+		context.Infof("Error while removing entity  %v", entity)
 		return err
 	}
 
-	if err := memcache.Delete(*context, key.String()); err != nil {
-		(*context).Infof("Error while removing entity from memcache %v", entity)
+	if err := memcache.Delete(context, key.String()); err != nil {
+		(context).Infof("Error while removing entity from memcache %v", entity)
 		return err
 	}
 	return nil
 
 }
 
-func Retrieve(context *appengine.Context, entity DatastoreEntity) error {
+func Retrieve(context appengine.Context, entity DatastoreEntity) error {
 
-	key := datastore.NewKey(*context, (entity).GetKind(), (entity).GetKey(), 0, nil)
+	key := datastore.NewKey(context, (entity).GetKind(), (entity).GetKey(), 0, nil)
 
 	//Try using memcache
-	if item, err := memcache.Get(*context, key.String()); err == nil {
+	if item, err := memcache.Get(context, key.String()); err == nil {
 		// Nice getting from memchache
 		if err := json.Unmarshal(item.Value, entity); err == nil {
 			return nil
 		}
-		(*context).Infof("Error while unserializing json from memcache")
+		context.Infof("Error while unserializing json from memcache")
 	}
 
-	if err := datastore.Get(*context, key, entity); err != nil {
-		(*context).Infof("No entity %v", entity)
+	if err := datastore.Get(context, key, entity); err != nil {
+		context.Infof("No entity %v", entity)
 		return err
 	}
 	return nil
 }
 
-func Store(context *appengine.Context, entity DatastoreEntity) error {
+func Store(context appengine.Context, entity DatastoreEntity) error {
 
-	key := datastore.NewKey(*context, (entity).GetKind(), (entity).GetKey(), 0, nil)
+	key := datastore.NewKey(context, (entity).GetKind(), (entity).GetKey(), 0, nil)
 
-	if _, err := datastore.Put(*context, key, entity); err != nil {
-		(*context).Infof("Error while storing entity %v", entity)
+	if _, err := datastore.Put(context, key, entity); err != nil {
+		context.Infof("Error while storing entity %v", entity)
 		return err
 	}
 
@@ -66,8 +66,8 @@ func Store(context *appengine.Context, entity DatastoreEntity) error {
 			Value: b,
 		}
 
-		if err := memcache.Set(*context, item); err != nil {
-			(*context).Infof("Error while storing in memcache %v", key)
+		if err := memcache.Set(context, item); err != nil {
+			context.Infof("Error while storing in memcache %v", key)
 		}
 	}
 
